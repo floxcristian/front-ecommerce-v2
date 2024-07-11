5e95614d57f5314bda5d650b7cb1b75795c63c67
Cristian Flores <cristianflores.ee@gmail.com>
2024-07-11 10:07:20 -0400

    se corrige responsive del header

--- src/app/modules/shared/layout/header/components/main-header-2/main-header-2.component.ts
+++ src/app/modules/shared/layout/header/components/main-header-2/main-header-2.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-main-header-2',
   standalone: true,
-  imports: [StyleClassModule, RippleModule],
+  imports: [StyleClassModule, RippleModule, BadgeModule, ButtonModule],
   templateUrl: './main-header-2.component.html',
   styleUrl: './main-header-2.component.scss',
 })

71541f11e6fdbe61896b70d0341804eb5622777c
Cristian Flores <cristianflores.ee@gmail.com>
2024-07-30 16:54:35 -0400

    se añade page signup

--- src/app/modules/signup/signup-page/signup-page.component.ts
+++ src/app/modules/signup/signup-page/signup-page.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+// Prime
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FileUploadModule } from 'primeng/fileupload';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextareaModule,
+    FileUploadModule,
+    DropdownModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+  ],
   templateUrl: './signup-page.component.html',
-  styleUrl: './signup-page.component.scss'
+  styleUrl: './signup-page.component.scss',
 })
 export class SignupPageComponent {
-
+  phoneCodes = ['+569', '+56'];
+  selectedPhoneCode = '+569';
 }

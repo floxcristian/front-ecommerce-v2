5a687ffceabe272342e19ab3b9f0ebd72a415042
Cristian Flores <cristianflores.ee@gmail.com>
2024-08-03 13:09:22 -0400

    se corrige SEO en footer y se cambia titulo rol

--- src/app/shared/layouts/footer/footer.component.ts
+++ src/app/shared/layouts/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RippleModule],
+  imports: [CommonModule, RippleModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })

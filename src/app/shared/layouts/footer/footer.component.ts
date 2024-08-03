60e042f68f7f00d5798f430b375fda735d73f7c6
Cristian Flores <cristianflores.ee@gmail.com>
2024-08-03 18:07:40 -0400

    se cambian los iconos

--- src/app/shared/layouts/footer/footer.component.ts
+++ src/app/shared/layouts/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BadgeModule } from 'primeng/badge';
 import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RippleModule, RouterModule],
+  imports: [CommonModule, RippleModule, RouterModule, BadgeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })

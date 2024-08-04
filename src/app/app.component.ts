d2796de4b324bd78c4c66eefbf6def47bdad939d
Cristian Flores <cristianflores.ee@gmail.com>
2024-08-04 16:14:34 -0400

    se añade ripple en primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,33 @@
 // Angular
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//
+// PrimeNg
 import { RippleModule } from 'primeng/ripple';
 import { StyleClassModule } from 'primeng/styleclass';
 import { InputTextModule } from 'primeng/inputtext';
 import { DividerModule } from 'primeng/divider';
 import { ButtonModule } from 'primeng/button';
 import { InputGroupModule } from 'primeng/inputgroup';
+import { PrimeNGConfig } from 'primeng/api';
+
+const PRIME_MODULES = [
+  RippleModule,
+  StyleClassModule,
+  InputTextModule,
+  DividerModule,
+  ButtonModule,
+  InputGroupModule,
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    RippleModule,
-    StyleClassModule,
-    InputTextModule,
-    DividerModule,
-    ButtonModule,
-    InputGroupModule,
-  ],
+  imports: [RouterOutlet, ...PRIME_MODULES],
   template: '<router-outlet/>',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'front-ecommerce-v2';
+  constructor(private primeConfig: PrimeNGConfig) {
+    this.primeConfig.ripple = true;
+  }
 }

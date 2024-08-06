392b8afaff3cbe28076d52042a982cc3474528c1
Cristian Flores <cristianflores.ee@gmail.com>
2024-08-06 17:43:55 -0400

    se corrigen environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
 export const environment = {
-  country: 'PE',
+  country: 'CL',
+  localeId: 'CLP',
+  currencyCode: 'CLP',
+  apiEcommerce: 'https://dev-api.implementos.cl/ecommerce',
+  /*country: 'PE',
   localeId: 'PEN',
   currencyCode: 'PEN',
   apiEcommerce: 'https://b2b-api.implementos.cl/ecommerce',
-  /*gmapScript:
-    'https://maps.googleapis.com/maps/api/js?key=AIzaSyD_HuwF5F8X8fOSR_1Ai_hFT115caUq4vI&libraries=places&callback=Function.prototype',*/
+  */
 };

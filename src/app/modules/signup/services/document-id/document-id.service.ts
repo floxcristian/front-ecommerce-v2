68bfd5663b8f2727127168b8efc150f9a9e917a8
Cristian Flores <cristianflores.ee@gmail.com>
2024-08-06 17:22:29 -0400

    se añaden restricciones rut

---
+++ src/app/modules/signup/services/document-id/document-id.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DocumentIdService {
+  constructor() {}
+
+  /**
+   * Dejar solo el número de documento editable quitando los caracteres especiales.
+   * @param documentId
+   * @returns
+   */
+  static getEditableValue(documentId: string): string {
+    if (!documentId) return '';
+    return documentId.replace(/\D/g, '');
+  }
+
+  static getValidCharacters(documentId: string): string {
+    if (!documentId) return '';
+    return documentId.replace(/[^0-9\K\k]/g, '');
+  }
+
+  static getFormattedDocumentId(documentId: string): string {
+    if (!documentId) return '';
+    const numbers = documentId.substring(0, documentId.length - 1);
+    const checksum = documentId.substring(documentId.length - 1);
+    return `${numbers}-${checksum}`;
+  }
+}

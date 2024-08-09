6b3991c953ddf2b45444f335897b67fea9269751
Cristian Flores <cristianflores.ee@gmail.com>
2024-08-09 12:48:48 -0400

    optimize component render

--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+// Angular
+import { Component, DestroyRef, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+// Rxjs
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
 // Components
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -11,4 +16,14 @@ import { FooterComponent } from '../footer/footer.component';
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+  private destroyRef = inject(DestroyRef);
+  private isSmallScreen$ = this.breakpointObserver
+    .observe(['(max-width: 991px)'])
+    .pipe(
+      takeUntilDestroyed(this.destroyRef),
+      map((result) => result.matches)
+    );
+  isSmallScreen = toSignal(this.isSmallScreen$, { initialValue: false });
+}

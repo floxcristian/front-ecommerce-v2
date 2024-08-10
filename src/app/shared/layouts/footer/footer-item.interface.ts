c17d33f9aa2f06feb251870dbb79a55fc15d7662
Cristian Flores <cristianflores.ee@gmail.com>
2024-08-10 00:32:29 -0400

    refactor footer items

---
+++ src/app/shared/layouts/footer/footer-item.interface.ts
@@ -0,0 +1,5 @@
+export interface IFooterItem {
+  label: string;
+  icon: string;
+  routerLink: string;
+}
--- src/app/shared/layouts/footer/footer.component.ts
+++ src/app/shared/layouts/footer/footer.component.ts
@@ -5,6 +5,8 @@ import { RouterModule } from '@angular/router';
 // PrimeNG
 import { BadgeModule } from 'primeng/badge';
 import { RippleModule } from 'primeng/ripple';
+// Models
+import { IFooterItem } from './footer-item.interface';
 
 const NG_MODULES = [CommonModule, RouterModule];
 const PRIME_MODULES = [BadgeModule, RippleModule];
@@ -16,4 +18,32 @@ const PRIME_MODULES = [BadgeModule, RippleModule];
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  items: IFooterItem[] = [
+    {
+      label: 'Inicio',
+      icon: 'fa-sharp fa-house',
+      routerLink: '/',
+    },
+    {
+      label: 'Categorías',
+      icon: 'fa-sharp fa-objects-column',
+      routerLink: '/1',
+    },
+    {
+      label: 'Mi cuenta',
+      icon: 'fa-sharp fa-user',
+      routerLink: 'auth/register',
+    },
+    {
+      label: 'Menú',
+      icon: 'fa-sharp fa-bars',
+      routerLink: '/2',
+    },
+    {
+      label: 'Carro',
+      icon: 'fa-sharp fa-cart-shopping ',
+      routerLink: '/3',
+    },
+  ];
+}
